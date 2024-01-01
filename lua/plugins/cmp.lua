3ad68897d4f4a87224e731ddbdf0e7d994addfda
Justinas Kavoliūnas <justinaskav@gmail.com>
2024-01-01 21:48:40 +0200

    Readd doc scrolling for cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -84,9 +84,8 @@ return {
           -- end),
           ['<C-n>'] = cmp.mapping.select_next_item(),
           ['<C-p>'] = cmp.mapping.select_prev_item(),
-          -- Disable because of indent issues
-          -- ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-          -- ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
           ['<C-Space>'] = cmp.mapping.complete(),
           ['<C-e>'] = cmp.mapping.close(),
           ['<CR>'] = cmp.mapping.confirm {
